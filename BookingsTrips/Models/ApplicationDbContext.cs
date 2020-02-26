02ee1711d9be775566f7435daa331ccd11f620b2
Ahmed Elhor <zizooo.elhor@gmail.com>
2020-02-26 13:44:48 +0200

    sperate db context

---
+++ BookingsTrips/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BookingsTrips.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string, IdentityUserLogin, IdentityUserRole, IdentityUserClaim>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+    }
+}
--- BookingsTrips/Models/IdentityModels.cs
+++ BookingsTrips/Models/IdentityModels.cs
@@ -40,18 +40,4 @@ namespace BookingsTrips.Models
         {
         }
     }
-
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string, IdentityUserLogin, IdentityUserRole, IdentityUserClaim>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection")
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-
-    }
 }
