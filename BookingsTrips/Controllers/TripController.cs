7d7eac9b952db6f2069207c5d2ed614f72054f4e
Ahmed Elhor <zizooo.elhor@gmail.com>
2020-03-14 15:35:40 +0200

    Add trip

--- BookingsTrips/Controllers/TripController.cs
+++ BookingsTrips/Controllers/TripController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using BookingsTrips.Models;
 using BookingsTrips.Models.ViewModels;
+using Microsoft.AspNet.Identity;
 
 namespace BookingsTrips.Controllers
 {
@@ -29,42 +30,101 @@ namespace BookingsTrips.Controllers
             return View(model.ToList());
         }
 
-        // GET: Trip/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Trip trip = db.Trips.Find(id);
-            if (trip == null)
-            {
-                return HttpNotFound();
-            }
-            return View(trip);
-        }
-
         // GET: Trip/Create
         public ActionResult Create()
         {
+            List<SelectListItem> flights = db.Flights.OrderByDescending(d => d.CreatedOn).Take(50).Where(t => t.TripId==null).ToList().Select(r => new SelectListItem()
+            {
+                Value = r.Id.ToString(),
+                Text = r.FromAirport + " إلى " + r.ToAirport + " - تاريخ " + r.FromDate.ToString("yyyy/MM/dd")
+            }).ToList();
+            ViewBag.Flights = flights;
+            List<SelectListItem> boats = db.Boats.OrderByDescending(d => d.CreatedOn).Take(50).Where(t => t.TripId == null).ToList().Select(r => new SelectListItem()
+            {
+                Value = r.Id.ToString(),
+                Text = "من تاريخ " + r.FromDate.ToString("yyyy/MM/dd") + " إلى تاريخ " + r.ToDate.ToString("yyyy/MM/dd")
+            }).ToList();
+            ViewBag.Boats = boats;
+
+            ViewBag.Tab = 0;
             return View();
         }
 
         // POST: Trip/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,StartPoint,EndPoint,FromDate,ToDate,Cost,AdultPrice,TeenPrice,ChildPrice,BabyPrice,IsActive,IsDeleted,CreatedBy,CreatedOn,EditedBy,EditedOn")] Trip trip)
+        public ActionResult Create(TripCreateViewModel model)
         {
             if (ModelState.IsValid)
             {
+                var trip = new Trip
+                {
+                    FromDate = model.FromDate,
+                    ToDate = model.ToDate,
+                    StartPoint = model.StartPoint,
+                    EndPoint = model.EndPoint,
+                    Cost = model.Cost,
+                    AdultPrice = model.AdultPrice,
+                    TeenPrice = model.TeenPrice,
+                    ChildPrice = model.ChildPrice,
+                    BabyPrice = model.BabyPrice,
+                    CreatedBy = User.Identity.GetUserId(),
+                    CreatedOn = DateTime.Now,
+                    EditedBy = User.Identity.GetUserId(),
+                    EditedOn = DateTime.Now
+                };
                 db.Trips.Add(trip);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+                var tripFlight = db.Flights.Find(model.FlightId);
+                tripFlight.Trip = trip;
+                db.Entry(tripFlight).State = EntityState.Modified;
 
-            return View(trip);
+                var tripBoat = db.Boats.Include(b => b.Floors).SingleOrDefault(i => i.Id == model.BoatId);
+                tripBoat.Trip = trip;
+                db.Entry(tripBoat).State = EntityState.Modified;
+
+                foreach (var floor in tripBoat.Floors)
+                {
+                    db.TripCabinsPrices.Add(new TripCabinsPrice
+                    {
+                        Trip = trip,
+                        Floor = floor,
+                        TripSingleCabinsPrice = 0,
+                        TripDoubleCabinsPrice = 0,
+                        TripTripleCabinsPrice = 0,
+                        CreatedBy = User.Identity.GetUserId(),
+                        CreatedOn = DateTime.Now,
+                        EditedBy = User.Identity.GetUserId(),
+                        EditedOn = DateTime.Now
+                    });
+                }
+                db.SaveChanges();
+
+                ViewData["Tab1Model"] = trip.TripCabinsPrices.Select(f => new TripCabinsPriceViewModel
+                {
+                    Id = f.Id,
+                    FloorNumber = f.Floor.FloorNumber,
+                    TripSingleCabinsPrice = f.TripSingleCabinsPrice,
+                    TripDoubleCabinsPrice = f.TripDoubleCabinsPrice,
+                    TripTripleCabinsPrice = f.TripTripleCabinsPrice
+                }).ToList();
+                ViewBag.Tab = 1;
+                TempData["alert"] = "<script>Swal.fire({icon: 'success', title: 'تم الحفظ بنجاح', showConfirmButton: false, timer: 1500})</script>";
+                return View();
+            }
+            List<SelectListItem> flights = db.Flights.OrderByDescending(d => d.CreatedOn).Take(50).Where(t => t.TripId == null).ToList().Select(r => new SelectListItem()
+            {
+                Value = r.Id.ToString(),
+                Text = r.FromAirport + " إلى " + r.ToAirport + " - تاريخ " + r.FromDate.ToString("yyyy/MM/dd")
+            }).ToList();
+            ViewBag.Flights = flights;
+            List<SelectListItem> boats = db.Boats.OrderByDescending(d => d.CreatedOn).Take(50).Where(t => t.TripId == null).ToList().Select(r => new SelectListItem()
+            {
+                Value = r.Id.ToString(),
+                Text = "من تاريخ " + r.FromDate.ToString("yyyy/MM/dd") + " إلى تاريخ " + r.ToDate.ToString("yyyy/MM/dd")
+            }).ToList();
+            ViewBag.Boats = boats;
+            ViewBag.Tab = 0;
+            return View(model);
         }
 
         // GET: Trip/Edit/5
@@ -83,8 +143,6 @@ namespace BookingsTrips.Controllers
         }
 
         // POST: Trip/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,StartPoint,EndPoint,FromDate,ToDate,Cost,AdultPrice,TeenPrice,ChildPrice,BabyPrice,IsActive,IsDeleted,CreatedBy,CreatedOn,EditedBy,EditedOn")] Trip trip)
@@ -97,31 +155,31 @@ namespace BookingsTrips.Controllers
             }
             return View(trip);
         }
-
-        // GET: Trip/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Trip trip = db.Trips.Find(id);
-            if (trip == null)
-            {
-                return HttpNotFound();
-            }
-            return View(trip);
-        }
-
-        // POST: Trip/Delete/5
-        [HttpPost, ActionName("Delete")]
+        
+        // POST: Boat/EditFloorCabinsCount
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public ActionResult EditTripCabinsPrice(IEnumerable<TripCabinsPriceViewModel> model)
         {
-            Trip trip = db.Trips.Find(id);
-            db.Trips.Remove(trip);
-            db.SaveChanges();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                foreach (var item in model)
+                {
+                    var floor = db.TripCabinsPrices.Find(item.Id);
+                    floor.TripSingleCabinsPrice = item.TripSingleCabinsPrice;
+                    floor.TripDoubleCabinsPrice = item.TripDoubleCabinsPrice;
+                    floor.TripTripleCabinsPrice = item.TripTripleCabinsPrice;
+                    floor.EditedBy = User.Identity.GetUserId();
+                    floor.EditedOn = DateTime.Now;
+                    db.Entry(floor).State = EntityState.Modified;                    
+                }
+                db.SaveChanges();
+                TempData["alert"] = "<script>Swal.fire({icon: 'success', title: 'تم الحفظ بنجاح', showConfirmButton: false, timer: 1500})</script>";
+                return RedirectToAction("Index");
+            }
+            ViewData["Tab1Model"] = model;
+            ViewBag.Tab = 1;
+            return View("Create");
         }
 
         protected override void Dispose(bool disposing)
@@ -132,5 +190,46 @@ namespace BookingsTrips.Controllers
             }
             base.Dispose(disposing);
         }
+
+        //// GET: Trip/Details/5
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Trip trip = db.Trips.Find(id);
+        //    if (trip == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(trip);
+        //}
+
+        //// GET: Trip/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Trip trip = db.Trips.Find(id);
+        //    if (trip == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(trip);
+        //}
+
+        //// POST: Trip/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    Trip trip = db.Trips.Find(id);
+        //    db.Trips.Remove(trip);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
     }
 }
--- BookingsTrips/Models/ViewModels/TripViewModels.cs
+++ BookingsTrips/Models/ViewModels/TripViewModels.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookingsTrips.Helper;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -25,5 +26,84 @@ namespace BookingsTrips.Models.ViewModels
         [DataType(DataType.Date)]
         [Display(Name = "التاريخ إلى")]
         public DateTime ToDate { get; set; }
+    }
+
+    public class TripCreateViewModel
+    {
+        [Required_AR]
+        [Display(Name = "الوجهة من")]
+        public string StartPoint { get; set; }
+
+        [Required_AR]
+        [Display(Name = "الوجهة إلى")]
+        public string EndPoint { get; set; }
+
+        [Required_AR]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
+        [DataType(DataType.Date)]
+        [Display(Name = "من تاريخ")]
+        public DateTime FromDate { get; set; }
+
+        [Required_AR]
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
+        [DataType(DataType.Date)]
+        [Display(Name = "إلى تاريخ")]
+        public DateTime ToDate { get; set; }
+
+        [Required_AR]
+        [RegularExpression(@"^[0-9]+(\.[0-9][0-9]?)?", ErrorMessage = "لابد من إدخال صيغة رقمية أو عشرية صحيحة !")]
+        [Display(Name = "سعر تكلفة الرحلة")]
+        public decimal Cost { get; set; }
+
+        [Required_AR]
+        [RegularExpression(@"^[0-9]+(\.[0-9][0-9]?)?", ErrorMessage = "لابد من إدخال صيغة رقمية أو عشرية صحيحة !")]
+        [Display(Name = "سعر برنامج الرحلة للبالغين")]
+        public decimal AdultPrice { get; set; }
+
+        [Required_AR]
+        [RegularExpression(@"^[0-9]+(\.[0-9][0-9]?)?", ErrorMessage = "لابد من إدخال صيغة رقمية أو عشرية صحيحة !")]
+        [Display(Name = "سعر برنامج الرحلة للأطفال أكبر من ست سنوات")]
+        public decimal TeenPrice { get; set; }
+
+        [Required_AR]
+        [RegularExpression(@"^[0-9]+(\.[0-9][0-9]?)?", ErrorMessage = "لابد من إدخال صيغة رقمية أو عشرية صحيحة !")]
+        [Display(Name = "سعر برنامج الرحلة للأطفال أصغر من ست سنوات")]
+        public decimal ChildPrice { get; set; }
+
+        [Required_AR]
+        [RegularExpression(@"^[0-9]+(\.[0-9][0-9]?)?", ErrorMessage = "لابد من إدخال صيغة رقمية أو عشرية صحيحة !")]
+        [Display(Name = "سعر برنامج الرحلة للأطفال الرضع")]
+        public decimal BabyPrice { get; set; }
+
+        [Required_AR]
+        [Range(1, int.MaxValue, ErrorMessage = "الطائرة مطلوب !")]
+        [Display(Name = "الطائرة")]
+        public int FlightId { get; set; }
+
+        [Required_AR]
+        [Range(1, int.MaxValue, ErrorMessage = "المركب مطلوب !")]
+        [Display(Name = "المركب")]
+        public int BoatId { get; set; }
+    }
+
+    public class TripCabinsPriceViewModel
+    {
+        public int Id { get; set; }
+        public int FloorNumber { get; set; }
+
+        [Required_AR]
+        [RegularExpression(@"^[0-9]+(\.[0-9][0-9]?)?", ErrorMessage = "لابد من إدخال صيغة رقمية أو عشرية صحيحة !")]
+        [Display(Name = "سعر الكبائن الفردي")]
+        public decimal TripSingleCabinsPrice { get; set; }
+
+        [Required_AR]
+        [RegularExpression(@"^[0-9]+(\.[0-9][0-9]?)?", ErrorMessage = "لابد من إدخال صيغة رقمية أو عشرية صحيحة !")]
+        [Display(Name = "سعر الكبائن الزوجي")]
+        public decimal TripDoubleCabinsPrice { get; set; }
+
+        [Required_AR]
+        [RegularExpression(@"^[0-9]+(\.[0-9][0-9]?)?", ErrorMessage = "لابد من إدخال صيغة رقمية أو عشرية صحيحة !")]
+        [Display(Name = "سعر الكبائن الثلاثي")]
+        public decimal TripTripleCabinsPrice { get; set; }
     }
 }
