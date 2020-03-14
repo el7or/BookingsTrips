5852d15d14d3f379437b4c2961ab4470236badb4
Ahmed Elhor <zizooo.elhor@gmail.com>
2020-03-14 12:08:55 +0200

    trips index

---
+++ BookingsTrips/Controllers/TripController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BookingsTrips.Models;
+using BookingsTrips.Models.ViewModels;
+
+namespace BookingsTrips.Controllers
+{
+    public class TripController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Trip
+        public ActionResult Index()
+        {
+            var model = db.Trips.OrderByDescending(d => d.CreatedOn).Take(50).Select(t => new TripIndexViewModel
+            {
+                Id = t.Id,
+                StartPoint = t.StartPoint,
+                EndPoint = t.EndPoint,
+                FromDate = t.FromDate,
+                ToDate = t.ToDate
+            });
+            return View(model.ToList());
+        }
+
+        // GET: Trip/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trip trip = db.Trips.Find(id);
+            if (trip == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trip);
+        }
+
+        // GET: Trip/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Trip/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,StartPoint,EndPoint,FromDate,ToDate,Cost,AdultPrice,TeenPrice,ChildPrice,BabyPrice,IsActive,IsDeleted,CreatedBy,CreatedOn,EditedBy,EditedOn")] Trip trip)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Trips.Add(trip);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(trip);
+        }
+
+        // GET: Trip/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trip trip = db.Trips.Find(id);
+            if (trip == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trip);
+        }
+
+        // POST: Trip/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,StartPoint,EndPoint,FromDate,ToDate,Cost,AdultPrice,TeenPrice,ChildPrice,BabyPrice,IsActive,IsDeleted,CreatedBy,CreatedOn,EditedBy,EditedOn")] Trip trip)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(trip).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(trip);
+        }
+
+        // GET: Trip/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trip trip = db.Trips.Find(id);
+            if (trip == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trip);
+        }
+
+        // POST: Trip/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Trip trip = db.Trips.Find(id);
+            db.Trips.Remove(trip);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BookingsTrips/Models/ViewModels/TripViewModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookingsTrips.Models.ViewModels
+{
+    public class TripIndexViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "الوجهة من")]
+        public string StartPoint { get; set; }
+
+        [Display(Name = "الوجهة إلى")]
+        public string EndPoint { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
+        [DataType(DataType.Date)]
+        [Display(Name = "التاريخ من")]
+        public DateTime FromDate { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
+        [DataType(DataType.Date)]
+        [Display(Name = "التاريخ إلى")]
+        public DateTime ToDate { get; set; }
+    }
+}
